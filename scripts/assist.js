e0ac0c4142f930df2d8a156c62f348087acd7e98
Sbee8777 <seb.bastiaans@hva.nl>
2026-01-12 12:28:33 +0100

    #20 hamburger menu open/close

--- scripts/assist.js
+++ scripts/assist.js
@@ -1,11 +1,23 @@
-const openBtn = document.querySelector('nav > button')
-const closeBtn = document.querySelector('.hamburgerMenu button')
-const menu = document.querySelector('.hamburgerMenu')
+// const openBtn = document.querySelector('nav > button')
+// const closeBtn = document.querySelector('.hamburgerMenu button')
+// const menu = document.querySelector('.hamburgerMenu')
 
-openBtn.addEventListener('click', function(){
-    menu.classList.add('hamburgerMenu-open')
-})
+// openBtn.addEventListener('click', function(){
+//     menu.classList.add('hamburgerMenu-open')
+// })
 
-closeBtn.addEventListener('click', function(){
-    menu.classList.remove('hamburgerMenu-open')
+// closeBtn.addEventListener('click', function(){
+//     menu.classList.remove('hamburgerMenu-open')
+// })
+
+const MenuBtn = document.querySelector('.menu-button')
+const menu = document.querySelector('.menu')
+// const path1 = document.querySelector('.l1')
+// const path2 = document.querySelector('.l2')
+// const path3 = document.querySelector('.l3')
+
+// console.log(hamburgerMenu)
+
+MenuBtn.addEventListener('click', function(){
+    menu.classList.toggle('hamburgerMenu-open')
 })
