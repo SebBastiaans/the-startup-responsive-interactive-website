aed82b98137065bc99d761774d9b113566e23ad0
Sbee8777 <seb.bastiaans@hva.nl>
2026-01-15 14:40:48 +0100

    menu gelabeled #20

--- scripts/assist.js
+++ scripts/assist.js
@@ -4,4 +4,9 @@ const menu = document.querySelector('.menu')
 MenuBtn.addEventListener('click', function(){
     menu.classList.toggle('hamburgerMenu-open')
     document.body.classList.toggle('menu-open')
+    if (document.body.classList.contains('menu-open')) {
+        document.querySelector('#menu-label').textContent = 'close'
+    } else {
+        document.querySelector('#menu-label').textContent = 'menu'
+    }
 })
