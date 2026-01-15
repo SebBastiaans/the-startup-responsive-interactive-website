22e3ef1ae186b10ccd350f6f981b034470c19dd5
Sbee8777 <seb.bastiaans@hva.nl>
2026-01-15 15:58:19 +0100

    #16 #20 menu accesible gemaakt. Aria, inert.

--- scripts/assist.js
+++ scripts/assist.js
@@ -6,7 +6,29 @@ MenuBtn.addEventListener('click', function(){
     document.body.classList.toggle('menu-open')
     if (document.body.classList.contains('menu-open')) {
         document.querySelector('#menu-label').textContent = 'close'
+        MenuBtn.setAttribute('aria-expanded', 'true')
+        menu.removeAttribute('inert')
     } else {
         document.querySelector('#menu-label').textContent = 'menu'
+        MenuBtn.setAttribute('aria-expanded', 'false')
+        menu.setAttribute('inert', '')
     }
 })
+
+// inert op ul alleen op mobile versie. https://www.youtube.com/watch?v=m7YDWNz65iI
+const media = window.matchMedia("(width < 1024px)")
+
+media.addEventListener('change', (e) => updateNav(e))
+
+function updateNav(e){
+    const isHamburger = e.matches
+    console.log(isHamburger)
+    if(isHamburger){
+        menu.setAttribute('inert', '')
+    }
+    else{
+        menu.removeAttribute('inert')
+    }
+}
+
+updateNav(media)
